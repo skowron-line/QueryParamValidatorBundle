1af575bb7ae9a18253fccc0cb1d3fff91616491c
krzysztof skaradzinski <wygladaszjak@gmail.com>
2016-04-26 21:07:17 +0200

    renamed bundle class name

--- SkowronlineQueryParamValidatorBundle.php
+++ SkowronlineQueryParamValidatorBundle.php
@@ -4,6 +4,6 @@ namespace Skowronline\QueryParamValidatorBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class SkowronlineQueryParamBundle extends Bundle
+class SkowronlineQueryParamValidatorBundle extends Bundle
 {
 }
